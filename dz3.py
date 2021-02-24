18f15cd8331932f87c4fbc98c6a15bef918769a0
Dukemegaman <Dukemegaman@gmail.com>
2021-02-24 20:44:35 +0300

    first lesson hw

---
+++ dz3.py
@@ -0,0 +1,11 @@
+a = input ('введите')
+print(f' {a} + {a + a} + {a + a + a} = {int(a) + int(a + a) + int(a + a + a)}')
+##b = int(a) + (int(a) + int(a)) + (int(a) + int(a) + int(a))
+
+a = input ('введите')
+print(f'{int(a) + int(a + a) + int(a + a + a)} ')
+
+
+a = input ('введите')
+b = int(a) + int(a + a) + int(a + a + a)
+print(f' {a} + {a + a} + {a + a + a} = {b}')
