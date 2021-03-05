e68210a02261ec7e56ead74f59252030e1a37640
Dukemegaman <Dukemegaman@gmail.com>
2021-03-05 23:22:56 +0300

    Second lesson HW

--- Lesson_2/dz2_5.py
+++ Lesson_2/dz2_5.py
@@ -1,8 +1,8 @@
 rating = [7, 5, 3, 3, 2]
-user_number = int(input("введите цифру "))
+user_number = int(input("Введите цифру "))
 per_n = 0
 for i in rating:
-    if user_number <= i
+    if user_number <= i:
      per_n += 1
 rating.insert(per_n, user_number)
-print(user_number)
+print(rating)
